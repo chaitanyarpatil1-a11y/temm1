1a2e71ec2c1767326239d7e339fe65ce427a68d5
chaitanyarpatil1-a11y <chaitanyarpatil1@gmail.com>
2026-07-13 10:34:29 +0530

    updated file

--- .ipynb_checkpoints/trial01-checkpoint.py
+++ .ipynb_checkpoints/trial01-checkpoint.py
@@ -1,1 +1,8 @@
-print("Hello World")
+print("Hello World")import random
+
+name = input("Enter your name: ")
+
+random_number = random.randint(1, 100)
+
+print("Hello", name)
+print("Your random number is:", random_number)
--- trial01.py
+++ trial01.py
@@ -1,1 +1,9 @@
-print("Hello World")
+print("Hello World")import random
+
+name = input("Enter your name: ")
+
+random_number = random.randint(1, 100)
+
+print("Hello", name)
+print("Your random number is:", random_number)
+print ("your got your answer below")
